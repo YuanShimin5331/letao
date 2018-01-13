c443a7158b0dcff70a823c8fa2648ab6dcf4db8e
Yuan Shiming <2982227640@qq.com>
2018-01-13 13:55:38 +0800

    登录页面完成

---
+++ public/pc/js/common.js
@@ -0,0 +1,50 @@
+
+$(function () {
+  
+  
+    //进度条功能
+    NProgress.configure({ showSpinner: false });//关闭进度环
+  
+    $(document).ajaxStart(function () {
+      //在发送ajax之前，要开启进度条，ajax结束后，要关闭进度条。
+      //使用进度条插件
+      NProgress.start();
+    });
+  
+    $(document).ajaxStop(function () {
+      //本地接口，加了一个延时
+      setTimeout(function () {
+        NProgress.done();
+      }, 1000);
+    });
+  
+  
+    //在非登陆页面，发送ajax请求，询问用户是否登录，如果没登录，跳转到登录页面。
+    if(location.href.indexOf("login.html") == -1){
+      $.ajax({
+        type:"get",
+        url:"/employee/checkRootLogin",
+        success:function (info) {
+          if(info.error == 400){
+            //说明没有登录，
+            location.href = "login.html";
+          }
+        }
+      });
+    }
+  
+  
+  
+  
+  
+
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  });
